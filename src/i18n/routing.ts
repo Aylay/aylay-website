1f0507200f6a85345d67d6c1150539e5f8be46b8
Aylay <lattali@beavers-agency.fr>
2026-06-25 23:38:59 +0200

    remove localDetection

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,5 +3,6 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["fr", "en"],
   defaultLocale: "fr",
-  localePrefix: "as-needed", // FR sans préfixe, EN sous /en
+  localePrefix: "as-needed",
+  localeDetection: false,
 });
